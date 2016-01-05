898041b64bd6a7fead472831924e85cf33fe4370
Tyler Thomann <Tyler Thomann>
2016-01-05 14:22:00 -0600

    got two functions to work

--- payperweekjavascript.js
+++ payperweekjavascript.js
@@ -2,13 +2,27 @@
 
 		function multiplytwoNumbers(hours,wage){
 			var result;
-			result = hours * wage
+			result= hours * wage;
 			return result;
-		};
-		var hours;
-		var wage;
+		}
+		var numhours;
+		var numwage;
 		var pay;
-		hours=40;
-		wage=23.43;
-		pay = multiplytwoNumbers(hours,wage);
-		console.log (pay);
+		numhours= 40;
+		numwage= 23.43;
+		pay=multiplytwoNumbers(numhours,numwage);
+		console.log(pay);
+		
+		function multiplytwoNumbers(overtimehours,overtimewage){
+			var result;
+			result= overtimehours * overtimewage;
+			return result;
+		}
+		var numoth;
+		var numotw;
+		var overtimepay;
+		numoth= 7;
+		numotw= 23.43 * 1.5;
+		overtimepay=multiplytwoNumbers(numoth,numotw);
+		console.log(overtimepay);
+		
